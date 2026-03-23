003c05e75c9fccd806802a0429ab8f15ca856c38
JoHn Pablo Javier Quinonez Paredes <pablo.6098gl@gmail.com>
2026-03-23 14:16:32 -0600

    array_merge()

--- funciones.php
+++ funciones.php
@@ -2,21 +2,21 @@
 //buscar funciones nativas de php meter todas en un archivo de php y comentar el titulo y que hace cada funcion...
 
 $numeros=[10,20,40,24,50,60,19,40,56];
-$frase="        Era_Un_Paracaidista_Que_Nunca_El_Miedo_Domino        ";
+$frase=["        Era_Un_Paracaidista_Que_Nunca_El_Miedo_Domino        "];
 
 //01. strlen()
 
-strlen($frase);
+strlen($frase[]);
 //Retorna la longitud de una cadena de texto qu este dentro del array
 
 //02. strtoloer()
 
-strtolower($frase);
+strtolower($frase[]);
 //Convierte texto a minúsculas 
 
 //03. strtoupper()
 
-strtoupper($frase);
+strtoupper($frase[]);
 //Convierte texto a mayúsculas
 
 //04. replace()
@@ -26,17 +26,18 @@ str_replace( "_"," ",$frase);
 
 //05. substr()
 
-substr( $frase, "0", "18" );
+substr( $frase[], "0", "18" );
 //Extrae una fragmento del array
 
 //06. strpos()
 
-strpos($frase, "Que" );
+strpos($frase[], "Que" );
 //Busca la posición de un texto dentro de otro
 
 //07. trim()
 
-trim("$frase");
+trim($frase[0]);
+
 //Elimina espacios al inicio y final
 
 //08. explode()
@@ -103,5 +104,8 @@ array_pop($numeros);
 
 //elimina el ultimo elemento que tiene un array
 
+//18. array_merge()
+
+array_merge($frase, $numeros);
 
 
